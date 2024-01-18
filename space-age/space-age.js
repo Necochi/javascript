11310d1404bd66764f1fe875a40a2e7e8f4c194a
Necochi <LikaGirl0@gmail.com>
2024-01-18 13:01:12 +0300

    Изменила неявное преобразование на явное

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -8,27 +8,27 @@ export const age = (planet, seconds) => {
 
   switch (planet) {
     case 'earth':
-      return +(seconds / earthTime / 1).toFixed(2);
+      return Number((seconds / earthTime / 1).toFixed(2));
 
     case 'mercury':
-      return +(seconds / earthTime /  0.2408467).toFixed(2);
+      return Number((seconds / earthTime /  0.2408467).toFixed(2));
 
     case 'venus':
-      return +(seconds / earthTime / 0.61519726).toFixed(2);
+      return Number((seconds / earthTime / 0.61519726).toFixed(2));
 
     case 'mars':
-      return +(seconds / earthTime / 1.8808158).toFixed(2);
+      return Number((seconds / earthTime / 1.8808158).toFixed(2));
 
     case 'jupiter':
-      return +(seconds / earthTime / 11.862615).toFixed(2);
+      return Number((seconds / earthTime / 11.862615).toFixed(2));
 
     case 'saturn':
-      return +(seconds / earthTime / 29.447498).toFixed(2);
+      return Number((seconds / earthTime / 29.447498).toFixed(2));
 
     case 'uranus':
-      return +(seconds / earthTime / 84.016846).toFixed(2);
+      return Number((seconds / earthTime / 84.016846).toFixed(2));
 
     case 'neptune':
-      return +(seconds / earthTime / 164.79132).toFixed(2);
+      return Number((seconds / earthTime / 164.79132).toFixed(2));
   };
 };
