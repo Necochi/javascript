34891e140790f8c0d571d0c6200464cbacf18136
Necochi <LikaGirl0@gmail.com>
2024-01-18 15:23:51 +0300

    Все тесты пройдены

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error('Remove this statement and implement this function');
+export const compute = (string1, string2) => {
+
+    let sum = 0;
+
+    if (string1.length != string2.length) {
+      throw new Error('strands must be of equal length'); 
+
+  } else if (string1.length === string2.length) {
+      for (let i = 0; i < string1.length; i++) {
+        if (string1[i] !== string2[i]) {
+          sum += 1
+        }
+      }
+      return sum;
+  }
 };
