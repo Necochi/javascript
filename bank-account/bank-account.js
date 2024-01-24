f27fb99eea880c12584df3459bac12e6e491355a
Necochi <LikaGirl0@gmail.com>
2024-01-24 22:29:58 +0300

    Пройдены все тесты

---
+++ bank-account/bank-account.js
@@ -0,0 +1,57 @@
+//
+// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+import { validateHeaderName } from "http";
+
+export class BankAccount {
+  constructor() {
+  }
+
+  open() {
+    if (this._balance !== 0) {
+    this._balance = 0;
+    } else {
+      throw new ValueError;
+    }
+  }
+
+  close() {
+    if (this._balance >= 0 ) {
+    this._balance = null;
+    } else {
+      throw new ValueError;
+    }
+  }
+
+  deposit(money) {
+    if (money > 0 && this._balance !== null) {
+    this._balance += money;
+    } else {
+      throw new ValueError;
+    }
+  }
+
+  withdraw(money) {
+    if (money > 0 && this._balance >= money ) {
+      this._balance -= money;
+    } else {
+      throw new ValueError;
+    }
+  }
+
+  get balance() {
+    if (this._balance === null) {
+      throw new ValueError
+    } else {
+      return this._balance;
+    }
+  }
+}
+
+export class ValueError extends Error {
+  constructor() {
+    super('Bank account error');
+  }
+}
